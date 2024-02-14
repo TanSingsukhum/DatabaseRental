b6270f41387a495e05fdb4e2836c09dff1f5ae4e
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2024-02-13 16:49:01 -0800

    SQL templating completed

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.locals.layout = false;
     ROUTES
 */
 
+
+app.get('/buildings', function(req,res){
+    let query = "SELECT * FROM Buildings";
+    db.pool.query(query, function(err, results) {
+        if (err) {
+            console.error("Error retrieving buildings:", err);
+            res.status(500).send("Error retrieving buildings");
+            return;
+        }
+        res.render('buildings', { buildings: results });
+    });
+})
+
 //Get table from hosts
 app.get('/hosts', function(req, res) {
     let query = "SELECT * FROM Hosts";
@@ -49,6 +62,43 @@ app.get('/clients', function(req,res){
             return;
         }
         res.render('clients', { clients: results });
+    });
+})
+
+app.get('/transactions', function(req,res){
+    let query = "SELECT * FROM Transactions";
+    db.pool.query(query, function(err, results) {
+        if (err) {
+            console.error("Error retrieving transactions:", err);
+            res.status(500).send("Error retrieving transactions");
+            return;
+        }
+        res.render('transactions', { transactions: results });
+    });
+})
+
+app.get('/rental_histories', function(req, res){
+    let query = "SELECT * FROM Rental_Histories"
+    db.pool.query(query, function(err, results) {
+        if (err) {
+            console.error("Error retrieving rental history:", err);
+            res.status(500).send("Error retrieving rental history");
+            return;
+        }
+        res.render('rental_histories', { rental_histories: results });
+    });
+})
+
+
+app.get('/reviews', function(req, res){
+    let query = "SELECT * FROM Reviews"
+    db.pool.query(query, function(err, results) {
+        if (err) {
+            console.error("Error retrieving reviews:", err);
+            res.status(500).send("Error retrieving reviews");
+            return;
+        }
+        res.render('reviews', { reviews: results });
     });
 })
 
